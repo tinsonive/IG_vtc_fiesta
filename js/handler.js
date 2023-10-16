af1a85782ba369d3b34b3c051ea2a2262b0724c6
Tinson <tinson0428@vtc.edu.hk>
2023-10-17 00:02:58 +0800

    Testing

--- js/handler.js
+++ js/handler.js
@@ -23,15 +23,10 @@ function statusChangeCallback(response) {
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
-        get_media_by_tag.then(
-            function(datalist) { medialist = datalist; },
-            function(code, msg) { console.log(msg); }
-        );
+        
     } else {
-        login.then(
-            function(response) { checkLoginState(); },
-            function(code, msg) { console.log(msg); }
-        );
+        
+      
     }
 }
 
@@ -48,104 +43,6 @@ window.fbAsyncInit = function() {
         xfbml            : true,
         version          : 'v18.0'
     });
-
-    login = new Promise(function(resolve, reject)
-        {
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    FB.api('/me', function(response) {
-                        if (response.error != null)
-                        reject(response.error.code, response.error.message);
-                        else
-                        resolve(response);
-                    });
-                } else { 
-                    //    <!-- If you are not logged in, the login dialog will open for you to login asking for permission to get your public profile and email -->
-                    reject(-1, 'User cancelled login or did not fully authorize.'); 
-                }
-            },{scope: [
-                'pages_show_list', 
-                'instagram_basic', 
-                'instagram_content_publish', 
-                'pages_read_engagement', 
-                'instagram_manage_insights'
-            ]});
-        }
-    );
-
-    get_busniess_id = new Promise(function(resolve, reject)
-    {
-      if (business_id != null)
-      {
-        resolve(business_id);
-        return;
-      }
-
-      FB.api('/'+PAGE_ID+'?fields=instagram_business_account', function(response) {
-          console.log(response);
-          if (response.error != null)
-            reject(response.error.code, response.error.message);
-          else
-          {
-            business_id = response.instagram_business_account.id;
-            resolve(business_id);
-          }
-      });
-    }
-  );
-
-  get_tag_id = new Promise(function(resolve, reject)
-    {
-      if (tag_id != null)
-      {
-        resolve(tag_id);
-        return;
-      }
-
-      if (business_id == null)
-        get_busniess_id.then(getTagID,reject);
-      else
-        getTagID(business_id);
-
-      function getTagID(bid) {
-        FB.api('ig_hashtag_search?user_id='+bid+'&q='+TAG_NAME, function(response) {
-          console.log(response);
-            if (response.error != null)
-              reject(response.error.code, response.error.message);
-            else
-            {
-                tag_id = response.data[0].id;
-                resolve(tag_id);
-            }
-        });
-      }
-    }
-  );
-
-  get_media_by_tag = new Promise(function(resolve, reject)
-    {
-      if (tag_id == null)
-      {
-        get_tag_id.then(getMedia, reject);
-      }
-      else
-      {
-        getMedia(tid);
-      }
-
-      function getMedia(tid) {
-        FB.api(tid+'/recent_media?user_id='+business_id+'&fields=id,media_type,media_url,timestamp', function(response) {
-          console.log(response);
-            if (response.error != null)
-              reject(response.error.code, response.error.message);
-            else
-            {
-                resolve(response.data);
-            }
-        });
-      }
-    }
-  ); 
 
   FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
