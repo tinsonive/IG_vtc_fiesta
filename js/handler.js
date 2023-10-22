8c459adcbf60aa26abedf66024d4ff3d8afd6bc2
Tinson <tinson0428@vtc.edu.hk>
2023-10-22 12:46:24 +0800

    Update handler.js

--- js/handler.js
+++ js/handler.js
@@ -223,8 +223,8 @@ const loopList = async () => {
   medialist = medialist.filter(item => (!!item.media_url));
 
   medialist.sort(function(a, b){
-      if (a.timestamp < b.timestamp) {return -1;}
-      if (a.timestamp > b.timestamp) {return 1;}
+      if (a.timestamp < b.timestamp) {return 1;}
+      if (a.timestamp > b.timestamp) {return -1;}
       return 0;
   });
 
